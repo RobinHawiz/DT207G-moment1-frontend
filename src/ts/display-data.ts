7eeca47ce0a8c55ba1b843a439f1927996921752
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2025-04-01 13:23:57 +0200

    🎉 init: scaffold project from existing repo
    - Copied core structure from the repo belonging to assignment DT208-moment1 to use as a foundation.
    - Will adapt and evolve this code to suit the purpose of the current assignment DT207-moment1.

---
+++ src/ts/display-data.ts
@@ -0,0 +1,36 @@
+import { ICourseInfo } from "@ts/ICourseInfo";
+
+// This function cannot be generic because it relies on properties specific to the ICourseInfo interface.
+export function displayDataList(data: Array<ICourseInfo>): void {
+  // Remove the previous tbody elem if it exists.
+  let prevTbody: HTMLTableSectionElement | null =
+    document.querySelector("tbody");
+  if (prevTbody) prevTbody.remove();
+
+  let table: HTMLTableElement = document.querySelector("table")!;
+  let tbody: HTMLTableSectionElement = document.createElement("tbody");
+
+  // Create a document fragment to batch DOM updates.
+  const fragment: DocumentFragment = document.createDocumentFragment();
+
+  data.forEach(({ code, coursename, progression, syllabus }) => {
+    const tr: HTMLTableRowElement = document.createElement("tr");
+    // Create an array excluding syllabus (we handle it separately).
+    [code, coursename, progression].forEach((text: string, index: number) => {
+      const td: HTMLTableCellElement = document.createElement("td");
+      if (index === 0) {
+        const a: HTMLAnchorElement = document.createElement("a");
+        a.innerText = text;
+        a.href = syllabus;
+        a.target = "_blank";
+        td.appendChild(a);
+      } else {
+        td.innerText = text;
+      }
+      tr.appendChild(td);
+    });
+    fragment.appendChild(tr);
+  });
+  tbody.appendChild(fragment);
+  table.appendChild(tbody);
+}
---
+++ src/ts/main.ts
@@ -0,0 +1,34 @@
+// Styles
+import "@styles/style.scss";
+// Interfaces
+import { ICourseInfo } from "@ts/ICourseInfo";
+// Classes
+import { DataList } from "@ts/DataList";
+// Functions
+import { fetchData } from "@ts/fetch-data";
+import { displayDataList } from "@ts/display-data";
+import { getLocalStorageData, updateLocalStorage } from "@ts/local-storage";
+import { initFormValidation } from "@ts/validate-form";
+import { processFormData } from "./process-form-data";
+
+async function main(): Promise<void> {
+  let data: DataList<ICourseInfo>;
+  let localStorageData: DataList<ICourseInfo> = new DataList(
+    getLocalStorageData<ICourseInfo>("Courses")
+  );
+  // Use local storage data if available, otherwise fetch new data.
+  if (localStorageData.getDataList().length !== 0) data = localStorageData;
+  else {
+    data = new DataList<ICourseInfo>(
+      await fetchData<Array<ICourseInfo>>(
+        "https://webbutveckling.miun.se/files/ramschema_ht24.json"
+      )
+    );
+    updateLocalStorage<ICourseInfo>("Courses", data.getDataList());
+  }
+
+  displayDataList(data.getDataList());
+  initFormValidation(processFormData);
+}
+
+main();
